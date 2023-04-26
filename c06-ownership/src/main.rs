279cf2202a44c12d314423e81954d697359b608b
Julio Valsesia <jvalsesia@gmail.com>
2023-04-26 19:39:58 -0300

    Adding Moving, cloning and Copying

--- c06-ownership/src/main.rs
+++ c06-ownership/src/main.rs
@@ -67,7 +67,49 @@ fn main() {
     // - waste memory
     // In Rust Ownership
     // - variables are responsible for freeing their own resources
-    
 
+    // Moving, cloning and copying data
+    // Moving
+    // "Mercury"    String is on the Heap
+    // inner_planet and outer_planet are both in the Stack
+    // 1st inner_planet is point to "Mercury" the Heap
+    // 2nd outer_planet is copying the inner_planet
+    // 3rd inner_planet is removed from Stack
+    let outer_planet: String;
+    {
+        let inner_planet = String::from("Mercury");
+        println!("inner_planet: {inner_planet}");
+        outer_planet = inner_planet;
+        // here inner_planet was moved and no longer exists
+        // println!("inner_planet: {inner_planet}");
+    }
+    println!("outer_planet: {outer_planet}");
 
+    // Cloning
+    let outer_planet_2: String;
+    {
+        let mut inner_planet_2 = String::from("Mercury");
+        println!("inner_planet_2: {inner_planet_2}");
+        // here inner_planet_2 was cloned and clear
+        outer_planet_2 = inner_planet_2.clone();
+        inner_planet_2.clear();
+        println!("inner_planet_2: {inner_planet_2}");
+    }
+    println!("outer_planet_2: {outer_planet_2}");
+
+    // Moving and Cloning does not work for Integer
+    // In Integers data types Rust just copy the
+
+    // Copying
+    let outer_planet_3: i32;
+    {
+        let mut inner_planet_3 = 4;
+        println!("inner_planet_3: {inner_planet_3}");
+        // here inner_planet_3 was copied because i32 does not have clone
+        // so i32, f64 do not use Heap... just Stack
+        outer_planet_3 = inner_planet_3;
+        inner_planet_3 += 1;
+        println!("inner_planet_3: {inner_planet_3}");
+    }
+    println!("outer_planet_3: {outer_planet_3}");
 }
