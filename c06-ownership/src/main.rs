59f85ebe01d196ceab9e195647fcf7e2f7de1214
Julio Valsesia <jvalsesia@gmail.com>
2023-04-26 18:21:30 -0300

    Adding Shadowing variables

--- c06-ownership/src/main.rs
+++ c06-ownership/src/main.rs
@@ -8,12 +8,25 @@ fn main() {
         let planet = "Earth";
         println!("planet is {planet}");
         println!("moon is {moon}");
-        
     }
     // after this block of code
     // planet not found in this scope
     // println!("planet is {planet}");
     // but moon is
     println!("planet is {moon}");
-        
+
+    // Shadowing variables
+    // Declare a new variable with the same name as an existing variable
+    // New variables "shadows" the previous one
+    let newPlanet = "Jupiter";
+    {
+        println!("newPlanet is {newPlanet}");
+        let newPlanet = "Saturn";
+        println!("newPlanet is {newPlanet}");
+        let mut newPlanet = 4; // mutability
+        println!("newPlanet is {newPlanet}");
+        newPlanet = 5;
+        println!("newPlanet is {newPlanet}");
+    }
+    println!("newPlanet is {newPlanet}");
 }
